ca58b7eacbeb95bd5dd0e1fa715ae973ca88ed59
Алёна Павлюкова <pavlyukova.av@rnd-mgn.ru>
2024-02-29 00:43:43 +0500

    Исправлено

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -13,13 +13,11 @@ func Unpack(str string) (string, error) {
 	if str == "" || str == " " {
 		return "", nil
 	}
-
 	if rune(str[0]) < 58 && rune(str[0]) > 47 {
 		return "", ErrInvalidString
 	}
 	var sb strings.Builder
 	var replaceStr, replaceLit string
-
 	for i, char := range str {
 		replaceStr = ""
 		if char == 48 { // если значение 0, то убираем предыдущий знак
@@ -28,14 +26,12 @@ func Unpack(str string) (string, error) {
 			sb.Reset()
 			sb.WriteString(s)
 		}
-
 		if str[i] < 58 && str[i] > 48 {
 			if i+1 != len(str) && str[i+1] < 58 && str[i+1] > 47 {
 				return "", ErrInvalidString
 			}
 			count, _ := strconv.Atoi(string(char))
 			replaceStr = strings.Repeat(replaceLit, count-1)
-
 		}
 		if replaceStr != "" {
 			sb.WriteString(replaceStr)
